5e2c464f3d872f8e12342b187d0b488c1386af71
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2022-08-18 00:01:18 +0700

    video8

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import configViewengine from './configs/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 3000;
 
+// setup view engine
 configViewengine(app);
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
 
-app.get('/about', (req, res) => {
-  res.send(`I'm key`)
-})
+// init web route
+initWebRoute(app);
+
+
 
 app.listen(port, () =>{
      console.log(`Example app listening on port ${port}`)
