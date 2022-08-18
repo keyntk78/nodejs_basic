bb7596e930031072efbf3154770ab5a8bc37b0b6
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2022-08-18 15:51:48 +0700

    video13

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -21,10 +21,32 @@ let createNewUser = async (req, res) => {
     return res.redirect('/');
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    console.log(userId);
+    await pool.execute('delete from users where id = ?', [userId])
+    return res.redirect('/');
+}
+
+let getEditPage = async (req, res) => {
+    let id = req.params.userId;
+    let [user] = await pool.execute('select * from users where id = ?', [id]);
+    return res.render('update.ejs', {dataUser:user[0]})
+}
+
+let updateUser = async (req, res) => {
+    let {firstName, lastName, email, address, id} = req.body;
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address =? where id = ?',
+    [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+}
 
 
 module.exports = {
     getHomePage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    getEditPage,
+    updateUser
 }
